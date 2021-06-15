9becefcfaa65c200d9603a0a8a1e0fe50b7df266
CyronF <cyronpfraser@gmail.com>
2021-06-15 18:43:26 -0400

    edited js to store data

--- db/STORE.js
+++ db/STORE.js
@@ -1,4 +1,6 @@
 const notesJSON = require("./db.json");
+const {writeFileSync} = require("fs");
+const path = require("path")
 
 //Stores notes
 
@@ -9,8 +11,10 @@ class Store {
     };
     addNote(note){
     const length = notesJSON.push(note);
-    return length -1;
+    writeFileSync(path.join(__dirname, "db.json"),JSON.stringify(notesJSON));
+    return length -1
     };
+ 
 };
 
 module.exports = new Store();
